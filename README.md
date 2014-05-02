5979227f381ad0b79cc3e03a6d89dfc5d999581b
Elliott Foster <elliottf@codebrews.com>
2014-05-02 11:26:47 -0500

    Adding drush parallel reload script.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Parallel Drush Reload
+
+Reloads Drupal databases using [drush](https://github.com/drush-ops/drush) and
+inspiration from [mysql-parallel](https://github.com/deviantintegral/mysql-parallel).
+
+### Dependencies
+
+* [GNU Parallel](http://www.gnu.org/software/parallel/)
+* [drush](https://github.com/drush-ops/drush)
+* [drush aliases](http://drush.ws/examples/example.aliases.drushrc.php)
+
+### Usage
+
+`./reload.sh @source.alias @dest.alis`
---
+++ reload.sh
@@ -0,0 +1,40 @@
+#!/bin/zsh
+
+drush=/Users/elliotttf/.composer/vendor/bin/drush
+parallel=/usr/local/bin/parallel
+source=$1
+dest=$2
+wd=`pwd`
+
+if [[ -z $source || -z $dest ]]; then
+  echo "Usage: reload.sh <source.alias> <dest.alias>"
+  exit 1
+fi
+
+$drush $dest sql-drop -y
+tables=`$drush $source sqlq "SHOW TABLES"`
+
+if [[ -z $tables ]]; then
+  echo "Unable to read tables from $source."
+  exit 1
+fi
+
+date=`date "+%Y-%m-%d-%H%M"`
+dumpDir=/tmp/$date-$source-$dest
+
+mkdir -p $dumpDir
+
+echo "========================================="
+echo "Dumping Tables From $source"
+echo "========================================="
+
+time echo $tables | $parallel -I %  echo "Dumping table %." \&\& $drush $source sql-dump --tables-list=% --gzip \> $dumpDir/%.sql.gz
+
+echo "========================================="
+echo "Importing tables to $dest"
+echo "========================================="
+
+cd $dumpDir
+time ls -S *.sql.gz | $parallel -I % echo "Importing table %." \&\& gzcat % \| $drush $dest sqlc
+cd $wd
+rm -rf $dumpDir
